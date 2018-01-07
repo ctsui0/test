fa4cff06a7e0da853395dafd4da888e7de3235a6
Zhiren Xu <zhirenxu@seniverse.com>
2018-01-07 13:28:45 +0800

    commit how.f90

---
+++ how.f90
@@ -0,0 +1,17 @@
+       Program how
+
+       implicit none 
+
+       integer a, b, c, d, e
+
+       a=3; b=6
+
+       c = a > b
+
+       d = a < b
+
+       print*, a > b
+
+       end program
+
+
